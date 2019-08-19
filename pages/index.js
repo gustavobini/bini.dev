04b6fb60c2b947eebd5bb28438652700102cd1d5
bini <gustavobini@gmail.com>
2019-08-18 22:48:25 -0300

    Add some info to the about page

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 
-import Nav from '../src/components/nav';
+import { Nav } from '../src/components/nav';
 import { Page } from '../src/layout/main';
 
 export default () => (
   <div>
     <Page />
-    <h1>bini.dev</h1>
     <Nav />
     <p>hi there 😊</p>
     <p>this is my playground - feel free to look around</p>
     <p>if you wanna get in touch, contact me @gustavobini on twitter</p>
     <style jsx>
       {`
-        h1 {
-          font-size: 2rem;
-          color: #ff5722;
-          text-align: right;
-        }
-
         p:first-of-type {
           font-size: 1.4rem;
           margin-top: 32px;
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,52 +1,74 @@
 import React from 'react';
 import Link from 'next/link';
 
-export default () => (
-  <nav>
-    <ul>
-      <li>
-        <a href="https://github.com/gustavobini">github</a>
-      </li>
-      <li>
-        <Link href="/blog">
-          <a>blog</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/about">
-          <a>about</a>
-        </Link>
-      </li>
-    </ul>
-    <style jsx>
-      {`
-        nav {
-          margin-top: 16px;
-        }
+export function Nav() {
+  return (
+    <header>
+      <Link href="/">
+        <a className="pagename">bini.dev</a>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <a
+              href="https://github.com/gustavobini"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              github
+            </a>
+          </li>
+          <li>
+            <Link href="/blog">
+              <a>blog</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>about</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <style jsx>
+        {`
+          a.pagename {
+            display: block;
+            font-size: 2rem;
 
-        ul {
-          display: flex;
-          justify-content: flex-end;
-          align-items: center;
-          margin: 0 auto;
-          min-width: 200px;
-          max-width: 800px;
-        }
+            color: #ff5722;
+            text-align: right;
+            text-decoration: none;
+          }
 
-        li {
-          font-size: 1.2rem;
-          list-style: none;
-        }
+          nav {
+            margin-top: 16px;
+          }
 
-        li:not(:first-of-type) {
-          margin-left: 16px;
-        }
+          ul {
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+            margin: 0 auto;
+            min-width: 200px;
+            max-width: 800px;
+          }
 
-        a {
-          color: #3f51b5;
-          font-weight: 900;
-        }
-      `}
-    </style>
-  </nav>
-);
+          li {
+            font-size: 1.2rem;
+            list-style: none;
+          }
+
+          li:not(:first-of-type) {
+            margin-left: 16px;
+          }
+
+          a {
+            color: #3f51b5;
+            font-weight: 900;
+          }
+        `}
+      </style>
+    </header>
+  );
+}
