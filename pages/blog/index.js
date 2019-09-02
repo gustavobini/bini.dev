9513cbc066e536d807478d88507e33c3f83b2329
bini <gustavobini@gmail.com>
2019-09-01 22:10:24 -0300

    Refactored it all to use mdx files

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import Link from 'next/link';
 
-import { Page } from '../../src/layout/main';
+export default function Blog(props) {
+  return props.posts.map((post, index) => (
+    <div>
+      <Link href={`/blog/posts/${post.url}`} key={index}>
+        <a>{post.title}</a>
+      </Link>
+    </div>
+  ));
+}
 
-export default () => (
-  <div>
-    <Page />
-    <p>there's nothing here yet :(</p>
-  </div>
-);
+function fileName(mdx) {
+  return mdx.substring(2, mdx.length - 4);
+}
+
+function requireAll(r) {
+  return r.keys().map(key => ({
+    content: r(key),
+    url: fileName(key)
+  }));
+}
+
+function requirePosts() {
+  return requireAll(require.context('./posts', true, /\.mdx$/));
+}
+
+function parsePosts(posts) {
+  return posts.map(post => {
+    return {
+      ...post.content.meta,
+      Post: post.content.default,
+      url: post.url
+    };
+  });
+}
+
+Blog.getInitialProps = async () => {
+  const posts = parsePosts(requirePosts());
+
+  return {
+    posts
+  };
+};
---
+++ src/layout/post.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function Post({ Doc }) {
+  return (
+    // <MDXProvider>
+    <div>
+      <Doc />
+    </div>
+    // </MDXProvider>
+  );
+}
